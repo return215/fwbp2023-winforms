d36c0488577201dac84794c48b408bb961a12c14
return215 <51498719+return215@users.noreply.github.com>
2023-09-02 04:15:06 -0700

    Add ScoringSystem

---
+++ WinForms-01/ScoringSystem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinForms_01
+{
+    internal class ScoringSystem
+    {
+        internal readonly record struct JudgeValue(string Name, double Value, bool Used = true)
+        {
+            public string Name { get; init; } = Name;
+            public double Value { get; init; } = Value;
+            public bool Used { get; init; } = Used;
+        }
+        public string Name { get; init; }
+        public List<JudgeValue> Judgements { get; set; }
+
+        public (double, double) CalculateScore(int[] judgeCounts) {
+            double score = 0;
+            double normalizedScore = 0;
+            double maxWeight = double.MinValue;
+            for (int i = 0; i < Judgements.Count; i++)
+            {
+                var judge = Judgements[i];
+                if (i >= judgeCounts.Length) break;
+                if (!judge.Used) continue;
+
+                maxWeight = Math.Max(maxWeight, judge.Value);
+                score += judge.Value * judgeCounts[i];
+            }
+            normalizedScore = score / maxWeight;
+            return (score, normalizedScore);
+        }
+
+    }
+}
